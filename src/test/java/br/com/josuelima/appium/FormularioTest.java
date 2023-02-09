9dc953aac992f0c516a08d535961791740d1666f
Josue-07 <josuelima875@gmail.com>
2023-02-09 04:48:15 -0300

    capturando alemntos[clicar, preecher]

--- src/test/java/br/com/josuelima/appium/FormularioTest.java
+++ src/test/java/br/com/josuelima/appium/FormularioTest.java
@@ -1,18 +1,24 @@
 package br.com.josuelima.appium;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FormularioTest {
 
     @Test
-    public void deveIstalarAPK() throws MalformedURLException {
+    public void devePreencherCampoTesxto() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("deviceName", "emulator-5554");
@@ -21,8 +27,18 @@ public class FormularioTest {
 //        desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
         desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:/Users/Public/workspace-QA/automation-appium-mob-project/src/test/resources/CTAppium_1_2.apk");
 
-        AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
+        AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        List<MobileElement> elements = driver.findElements(By.className("android.widget.TextView"));
+        elements.get(1).click();
+
+       MobileElement campoNome = driver.findElement(MobileBy.AccessibilityId("nome"));
+       campoNome.sendKeys("Josué Lima");
+
+        String text = campoNome.getText();
+
+        Assert.assertEquals("Josué Lima", text);
 
         driver.quit();
     }
