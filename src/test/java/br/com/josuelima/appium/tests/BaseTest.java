fb2e99723ee6f38fd8e95c05c75ebbf3e1004fe9
Josue-07 <josuelima875@gmail.com>
2023-02-17 04:13:29 -0300

    subiadicionando screeshots ao final de cada teste

--- src/test/java/br/com/josuelima/appium/tests/BaseTest.java
+++ src/test/java/br/com/josuelima/appium/tests/BaseTest.java
@@ -1,12 +1,23 @@
 package br.com.josuelima.appium.tests;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
 
 import static br.com.josuelima.core.DriverFactory.getDriver;
 import static br.com.josuelima.core.DriverFactory.killDriver;
 
 public class BaseTest {
+
+    @Rule
+    public TestName testName = new TestName();
 
     @AfterClass
     public static void finalizarClasse() {
@@ -16,6 +27,17 @@ public class BaseTest {
 
     @After
     public void tearDown() {
+        capiturarScreenShot();
         getDriver().resetApp();
+    }
+
+    public void capiturarScreenShot() {
+        try {
+            File img = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(img, new File("target/screeshots/" + testName.getMethodName() + ".png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
